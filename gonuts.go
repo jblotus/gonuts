3d272f8c823d0b8704437720672f664617e98e78
James Fuller <jblotus@gmail.com>
2014-05-13 01:09:10 +0000

    added a simple server loop

--- gonuts.go
+++ gonuts.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
 import "./timeserver/"
 
 func main() {
-  timeserver.NewServer();
+    
+    for true {
+        fmt.Println("Press 1 to launch timeserver")
+
+        var choice string
+
+        _, err := fmt.Scanln(&choice)
+
+        if err != nil {
+            fmt.Println("Error: ", err)
+        }
+
+        if choice == "1" {
+            timeserver.NewServer();   
+        } else {
+            fmt.Println("That is not a valid choice")
+        }
+    }
+    
 }
